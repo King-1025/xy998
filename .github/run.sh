28dbacb35e96fb72343e24986251053f00830a16
King-1025 <1543641386@qq.com>
2020-02-02 16:54:16 +0800

    添加.github

---
+++ .github/run.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+ROOT=$(pwd)
+RELEASE=$ROOT/../app/build/outputs/apk
+OUTPUT=$ROOT/output
+KS=$ROOT/extra/debug.jks
+KSP=$ROOT/extra/debug_ks_pass
+
+pip install requests
+sudo apt install apksigner -y
+
+$ROOT/../gradlew :app:build
+
+mkdir $OUTPUT
+
+for i in $(find $RELEASE -name *.apk -print); do
+   in=$i
+   out="$OUTPUT/$(echo $(basename $i) | sed "s/un//g")"
+   apksigner sign -v --ks $KS --in $in --out $out --ks-pass file:$KSP
+done
+
+$ROOT/extra/esc 18749089524 12345678x $OUTPUT
